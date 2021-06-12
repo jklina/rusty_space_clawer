04f4342ad9c32e82ab8a5d0c7a07c14de15b6a6a
Josh Klina <joshua.klina@gmail.com>
2021-06-12 15:58:28 -0600

    Add basic loop

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,63 @@
-// use hyper::body::HttpBody as _;
-// use hyper::Client;
-// use tokio::io::{self, AsyncWriteExt as _};
-
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-//     // This is where we will setup our HTTP client requests.
-//     let client = Client::new();
-//
-//     // Parse an `http::Uri`...
-//     let uri = "http://google.com".parse()?;
-//
-//     // Await the response...
-//     let mut resp = client.get(uri).await?;
-//
-//     println!("Response: {}", resp.status());
-//
-//     while let Some(chunk) = resp.body_mut().data().await {
-//         io::stdout().write_all(&chunk?).await?;
-//     }
-//
-//     Ok(())
-// }
-//
-use std::env;
+use std::io;
 
 enum Command<'a> {
     Login { username: &'a str, password: &'a str },
     Locations,
     Contracts,
     Players,
+    Exit,
     Undefined,
 }
 
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command: Command;
-    println!("{:?}", args);
+    loop {
+        let mut user_input = String::new();
+        let command: Command;
 
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
 
-    command = match args.len() {
-        2 => {
-            match &args[1][..] {
-                "locations" => Command::Locations,
-                "contracts" => Command::Contracts,
-                "players" => Command::Players,
-                _ => Command::Undefined,
+        let parsed_user_input: Vec<&str> = user_input.trim().split(" ").collect();
+        println!("{:?}", parsed_user_input);
+        command = match parsed_user_input.len() {
+            1 => {
+                match &parsed_user_input[0][..] {
+                    "locations" => Command::Locations,
+                    "contracts" => Command::Contracts,
+                    "players" => Command::Players,
+                    "exit" => Command::Exit,
+                    _ => Command::Undefined,
+                }
+            },
+            3 => {
+                match &parsed_user_input[0][..] {
+                    "login" => Command::Login { username: &parsed_user_input[1], password: &parsed_user_input[2] },
+                    _ => Command::Undefined,
+                }
+            },
+            _ => Command::Undefined,
+        };
+
+        match command {
+            Command::Login { username, password } => {
+                println!("Logging in {:?} {:?} !", username, password);
             }
-        },
-        4 => {
-            match &args[1][..] {
-                "login" => Command::Login { username: &args[2], password: &args[3] },
-                _ => Command::Undefined,
+            Command::Locations => {
+                println!("Get locations!");
             }
-        },
-        _ => Command::Undefined,
-    };
-
-    match command {
-        Command::Login { username, password } => {
-            println!("Logging in {:?} {:?} !", username, password);
-        }
-        Command::Locations => {
-            println!("Get locations!");
-        }
-        Command::Contracts => {
-            println!("Get contracts!");
-        }
-        Command::Players => {
-            println!("Get players!");
-        }
-        Command::Undefined => {
-            println!("Unknown!");
+            Command::Contracts => {
+                println!("Get contracts!");
+            }
+            Command::Players => {
+                println!("Get players!");
+            }
+            Command::Undefined => {
+                println!("Unknown!");
+            }
+            Command::Exit => {
+                break;
+            }
         }
     }
 }
